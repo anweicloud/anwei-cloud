2e4f15dbc47eb4af28c56792525f5ccffb7b9cd9
Anweicloud <d7210c@163.com>
2019-07-18 00:28:43 +0800

    fisrt commit

--- nacos-discovery-openfeign/src/main/java/com/anwei/cloud/nacos/discovery/openfeign/controller/FeignController.java
+++ nacos-discovery-openfeign/src/main/java/com/anwei/cloud/nacos/discovery/openfeign/controller/FeignController.java
@@ -1,14 +1,15 @@
 package com.anwei.cloud.nacos.discovery.openfeign.controller;
 
 import com.anwei.cloud.nacos.discovery.openfeign.service.FeignService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class FeignController {
 
-    @Autowired
+    @Resource
     FeignService service;
 
     @RequestMapping("test")
